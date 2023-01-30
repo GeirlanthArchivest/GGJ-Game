798de05ff5c0bd5d3ae7a2ff7f62962e9bfedb47
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2023-01-30 21:17:00 +0000

    Added second character and better camera controls
    camera controls include ability to change between cameras one the two different characters with the 1 and 2 keys

--- GGJ Game/Assets/Scripts/FollowPlayer.cs
+++ GGJ Game/Assets/Scripts/FollowPlayer.cs
@@ -11,12 +11,16 @@ public class FollowPlayer : MonoBehaviour
     }
 
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 2, -7);
+    public float distanceFromPlayer = 5;
+    public float height = 2;
+    //private Vector3 offset = new Vector3(0, 2, -7);
 
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position - player.transform.forward * distanceFromPlayer;
+        transform.LookAt(player.transform.position);
+        transform.position = new Vector3(transform.position.x, transform.position.y + height, transform.position.z);
     }
 }
