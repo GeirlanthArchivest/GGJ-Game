773e7119adda4e7e0e8eea21de06d8826084f5de
GeirlanthArchivest <100598359+GeirlanthArchivest@users.noreply.github.com>
2023-01-31 11:45:10 +0000

    Added test dungeon

---
+++ GGJ Game/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f;
+    public Transform[] waypoints;
+    int waypointsIndex;
+    Vector3 target2;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerManager.instance.Player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance2 = Vector3.Distance(target.position, transform.position);
+
+        if (distance2 <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+        else
+        {
+            Debug.Log("Else Initiated");
+            if (Vector3.Distance(transform.position, target2) < 4)
+            {
+                Debug.Log("Patrol reinitiated");
+                IterateWaypointIndex();
+                UpdateDestination();
+            }
+        }
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    void UpdateDestination()
+    {
+        target2 = waypoints[waypointsIndex].position;
+        agent.SetDestination(target2);
+    }
+
+    void IterateWaypointIndex()
+    {
+        waypointsIndex++;
+        if (waypointsIndex == waypoints.Length)
+        {
+            waypointsIndex = 0;
+        }
+    }
+}
